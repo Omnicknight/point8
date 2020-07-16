65fa5dd9b9e3b49d7e2f72b313267eec3ceaabc6
rtmkibish <rtmkibish@gmail.com>
2020-07-16 20:52:22 +0300

    semi

--- script.js
+++ script.js
@@ -13,17 +13,37 @@ const kettle = {
     shuffle() {
         console.log('Shuffling..');
     },
-    addToIngridients(one, two) {
-        this.ingridients[one] = two;
+    addToIngridients(key, value) {
+        console.log('Adding...', key, value);
+        this.ingridients[key] = value;
+        this.shuffle();
     },
 };
 
 kettle.spellTheCast = function() {
     console.log(this.material);
-    this.shuffle()
+    this.shuffle();
+};
+
+// kettle.addToIngridients('ing3', 'teeth');
+// kettle.addToIngridients('ing4', 'banana');
+// kettle.addToIngridients('ing5', 'poo');
+
+// const pick = (obj, key) => {
+//     return obj[key];
+// }
+
+const cat = {
+    material: 'abc',
+    spellTheCast () {
+        const self = this;
+        const fnc = function () {
+            return self.material;
+        };
+        alert(fnc());
+    },
 };
 
 kettle.spellTheCast();
-kettle.addToIngridients('ing3', 'teeth');
-console.log(kettle.ingridients.ing3);
 
+ 
